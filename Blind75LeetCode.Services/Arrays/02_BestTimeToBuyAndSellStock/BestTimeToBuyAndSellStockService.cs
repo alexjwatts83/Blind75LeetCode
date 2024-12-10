9dc746f57fbaff873b9366b3014c929737b3b483
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-10 20:38:13 +1100

    working

--- Blind75LeetCode.Services/Arrays/02_BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockService.cs
+++ Blind75LeetCode.Services/Arrays/02_BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockService.cs
@@ -10,8 +10,23 @@ public class BestTimeToBuyAndSellStockService
     public static int Run(int[] nums)
     {
         var answer = 0;
-        // loop through the numbers
 
+        // loop through the numbers
+        Dictionary<int, int> map = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var biggest = -1;
+            var value = nums[i];
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                var secondValue = nums[j];
+                var diff = secondValue - value;
+                if (diff > biggest)
+                    biggest = diff;
+            }
+            if (biggest > answer)
+                answer = biggest;
+        }
 
         return answer;
     }
