19c450b6a235d1f2ad2532ccda0b8b77009e81c3
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-10 20:32:07 +1100

    added test but fails

--- Blind75LeetCode.Services/Arrays/02_BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockService.cs
+++ Blind75LeetCode.Services/Arrays/02_BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockService.cs
@@ -7,5 +7,12 @@ using System.Threading.Tasks;
 namespace Blind75LeetCode.Services.Arrays._02_BestTimeToBuyAndSellStock;
 public class BestTimeToBuyAndSellStockService
 {
+    public static int Run(int[] nums)
+    {
+        var answer = 0;
+        // loop through the numbers
 
+
+        return answer;
+    }
 }
---
+++ Blind75LeetCode.UnitTests/Arrays/02_BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockTests.cs
@@ -0,0 +1,26 @@
+﻿using Blind75LeetCode.Services.Arrays._02_BestTimeToBuyAndSellStock;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blind75LeetCode.UnitTests.Arrays._02_BestTimeToBuyAndSellStock;
+
+public class BestTimeToBuyAndSellStockTests
+{
+    [Fact]
+    public void Optimised()
+    {
+        // Arrange
+        var nums = new int[] { 7, 1, 5, 3, 6, 4 };
+        var answer = 5;
+
+        // Act
+        var result = BestTimeToBuyAndSellStockService.Run(nums);
+
+        // Assert
+        result.ShouldBe(answer);
+    }
+}
