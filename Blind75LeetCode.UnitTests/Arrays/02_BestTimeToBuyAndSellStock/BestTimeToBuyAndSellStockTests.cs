6a9b12c9ffdd5b97c243080380c00d7544a9bff8
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-11 19:59:20 +1100

    added test data for best time to buy and sell stocks test

--- Blind75LeetCode.UnitTests/Arrays/02_BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockTests.cs
+++ Blind75LeetCode.UnitTests/Arrays/02_BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockTests.cs
@@ -10,17 +10,22 @@ namespace Blind75LeetCode.UnitTests.Arrays._02_BestTimeToBuyAndSellStock;
 
 public class BestTimeToBuyAndSellStockTests
 {
-    [Fact]
-    public void Optimised()
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void Optimised(int[] nums, int answer)
     {
         // Arrange
-        var nums = new int[] { 7, 1, 5, 3, 6, 4 };
-        var answer = 5;
-
         // Act
         var result = BestTimeToBuyAndSellStockService.Run(nums);
 
         // Assert
         result.ShouldBe(answer);
     }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { new int[] { 7, 1, 5, 3, 6, 4 }, 5 },
+            new object[] { new int[] { 7, 6, 4, 3, 1 }, 0 },
+        };
 }
