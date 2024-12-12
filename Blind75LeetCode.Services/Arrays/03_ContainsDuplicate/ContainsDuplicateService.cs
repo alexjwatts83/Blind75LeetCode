874392f0f314d57cb4e8d90e2bce8a8e44c26fff
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-12 20:36:48 +1100

    Brute force solution done

--- Blind75LeetCode.Services/Arrays/03_ContainsDuplicate/ContainsDuplicateService.cs
+++ Blind75LeetCode.Services/Arrays/03_ContainsDuplicate/ContainsDuplicateService.cs
@@ -11,6 +11,20 @@ public class ContainsDuplicateService
     {
         var containsDuplicates = false;
 
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (i == j)
+                    continue;
+
+                if (nums[i] == nums[j])
+                    containsDuplicates = true;
+            }
+            if (containsDuplicates)
+                break;
+        }
+
         return containsDuplicates;
     }
 }
