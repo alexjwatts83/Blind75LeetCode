74dcbcea495880b864015be43a227f8968a6a869
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-12 20:29:22 +1100

    setting up the tests

---
+++ Blind75LeetCode.UnitTests/Arrays/03_ContainsDuplicate/ContainsDuplicateTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blind75LeetCode.UnitTests.Arrays._03_ContainsDuplicate;
+
+public class ContainsDuplicateTests
+{
+    [Fact]
+    public void BruteForceTests()
+    {
+
+    }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { new int[] { 1, 2, 1, 2, 3 }, true },
+            new object[] { new int[] { 1, 2, 3, 1 }, true },
+            new object[] { new int[] { 1, 1, 1, 3, 3, 4 }, true },
+            new object[] { new int[] { 1, 3, 4, 2 }, false },
+        };
+}
