7c201272aaa287ecf479d9c38fffa1d68cef6c02
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-13 20:19:18 +1100

    Product Of Array Except Self setup

---
+++ Blind75LeetCode.Services/Arrays/04_ProductOfArrayExceptSelf/ProductOfArrayExceptSelfService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blind75LeetCode.Services.Arrays._04_ProductOfArrayExceptSelf;
+
+public class ProductOfArrayExceptSelfService
+{
+    public static int[] BruteForce(int[] nums)
+    {
+        var answer = new int[] { };
+
+        return answer;
+    }
+}
---
+++ Blind75LeetCode.UnitTests/Arrays/04_ProductOfArrayExceptSelf/ProductOfArrayExceptSelfTests.cs
@@ -0,0 +1,26 @@
+﻿using Blind75LeetCode.Services.Arrays._04_ProductOfArrayExceptSelf;
+using Shouldly;
+
+namespace Blind75LeetCode.UnitTests.Arrays._04_ProductOfArrayExceptSelf;
+
+public class ProductOfArrayExceptSelfTests
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void OptimisedSortedTests(int[] nums, int[] answer)
+    {
+        // Arrange
+        // Act
+        var result = ProductOfArrayExceptSelfService.BruteForce(nums);
+
+        // Assert
+        result.ShouldBe(answer);
+    }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { new int[] { 1, 2, 3, 4 }, new int[] { 24, 12, 8, 6 } },
+            new object[] { new int[] { -1, -1, 0, -3, 3 }, new int[] { 0, 0, 9, 0, 0 } },
+        };
+}
