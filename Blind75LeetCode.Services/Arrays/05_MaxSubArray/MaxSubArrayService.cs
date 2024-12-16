d2e16ce938ec14807fd03952a9d358f36eb41dff
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-16 20:13:52 +1100

    setup

---
+++ Blind75LeetCode.Services/Arrays/05_MaxSubArray/MaxSubArrayService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blind75LeetCode.Services.Arrays._05_MaxSubArray;
+public class MaxSubArrayService
+{
+    public static int[] BruteForce(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+
+            }
+        }
+
+        return nums;
+    }
+}
---
+++ Blind75LeetCode.UnitTests/Arrays/05_MaxSubArray/MaxSubArrayTests.cs
@@ -0,0 +1,33 @@
+﻿using Blind75LeetCode.Services.Arrays._04_ProductOfArrayExceptSelf;
+using Blind75LeetCode.Services.Arrays._05_MaxSubArray;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blind75LeetCode.UnitTests.Arrays._05_MaxSubArray;
+
+public class MaxSubArrayTests
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void BruteForceTests(int[] nums, int[] answer)
+    {
+        // Arrange
+        // Act
+        var result = MaxSubArrayService.BruteForce(nums);
+
+        // Assert
+        result.ShouldBe(answer);
+    }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { new int[] { 1, 2, 3, 4 }, new int[] { 24, 12, 8, 6 } },
+        };
+}
+
+
