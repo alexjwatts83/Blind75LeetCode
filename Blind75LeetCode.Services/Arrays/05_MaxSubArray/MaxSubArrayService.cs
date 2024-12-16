36c3fd9d8c849750d613675672a8b0a4716c0307
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-16 20:30:09 +1100

    brain dump checkin

--- Blind75LeetCode.Services/Arrays/05_MaxSubArray/MaxSubArrayService.cs
+++ Blind75LeetCode.Services/Arrays/05_MaxSubArray/MaxSubArrayService.cs
@@ -3,6 +3,8 @@ public class MaxSubArrayService
 {
     public static int[] BruteForce(int[] nums)
     {
+        // [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+        // [-2, -1, -4, 0, -1, 1, 2, -3, 0
         var startIndex = 0;
         var endIndex = 0;
         var sum = nums[0];
@@ -10,12 +12,20 @@ public class MaxSubArrayService
         {
             // i = 1
             // sum = -2
+            // curSum = -2 + 1 = -1
+
+            // i = 2
+            // sum = -1
+            // curSum = -1 + (-3) = -4
             var curSum = nums[i] + sum;
             if (curSum > sum)
             {
                 sum = curSum;
+                endIndex = i;
                 continue;
             }
+            startIndex = i;
+            sum = nums[i];
 
             for (int j = 1; j < nums.Length; j++)
             {
