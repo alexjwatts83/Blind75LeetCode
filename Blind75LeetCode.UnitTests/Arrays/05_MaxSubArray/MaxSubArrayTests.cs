36d1e26be12b2afd5aba94d7b27e68214de8f61d
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-17 20:51:16 +1100

    Added more max sub array tests

--- Blind75LeetCode.UnitTests/Arrays/05_MaxSubArray/MaxSubArrayTests.cs
+++ Blind75LeetCode.UnitTests/Arrays/05_MaxSubArray/MaxSubArrayTests.cs
@@ -21,6 +21,9 @@ public class MaxSubArrayTests
         new List<object[]>
         {
             new object[] { new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }, 6 },
+            new object[] { new int[] { 2, 3, -8, 7, -1, 2, 3 }, 11 },
+            new object[] { new int[] { -2, -4 }, -2 },
+            new object[] { new int[] { 5, 4, 1, 7, 8 }, 25 },
         };
 }
 
