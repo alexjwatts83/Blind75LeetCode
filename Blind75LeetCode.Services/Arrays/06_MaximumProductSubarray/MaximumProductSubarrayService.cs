063bb376581c665a0e41680910c09978c2437cff
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-18 19:18:38 +1100

    setup

---
+++ Blind75LeetCode.Services/Arrays/06_MaximumProductSubarray/MaximumProductSubarrayService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blind75LeetCode.Services.Arrays._06_MaximumProductSubarray;
+public static class MaximumProductSubarrayService
+{
+    public static int BruteForce(int[] nums)
+    {
+        var answer = 0;
+
+        return answer;
+    }
+}
---
+++ Blind75LeetCode.UnitTests/Arrays/06_MaximumProductSubarray/MaximumProductSubarrayTests.cs
@@ -0,0 +1,30 @@
+﻿using Blind75LeetCode.Services.Arrays._05_MaxSubArray;
+using Blind75LeetCode.Services.Arrays._06_MaximumProductSubarray;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blind75LeetCode.UnitTests.Arrays._06_MaximumProductSubarray;
+public class MaximumProductSubarrayTests
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void OptimisedTests(int[] nums, int answer)
+    {
+        // Arrange
+        // Act
+        var result = MaximumProductSubarrayService.BruteForce(nums);
+
+        // Assert
+        result.ShouldBe(answer);
+    }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { new int[] { -2, -4 }, -2 },
+        };
+}
