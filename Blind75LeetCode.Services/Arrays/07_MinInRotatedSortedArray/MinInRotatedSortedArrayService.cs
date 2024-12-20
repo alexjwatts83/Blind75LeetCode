dd717e9c26717e521f3c228c7693953eda503e17
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-20 15:24:11 +1100

    setup checkin

---
+++ Blind75LeetCode.Services/Arrays/07_MinInRotatedSortedArray/MinInRotatedSortedArrayService.cs
@@ -0,0 +1,12 @@
+﻿namespace Blind75LeetCode.Services.Arrays._07_MinInRotatedSortedArray;
+public static class MinInRotatedSortedArrayService
+{
+    public static int BruteForce(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+        }
+        return 0;
+    }
+}
---
+++ Blind75LeetCode.UnitTests/Arrays/07_MinInRotatedSortedArray/MinInRotatedSortedArrayTests.cs
@@ -0,0 +1,24 @@
+﻿using Blind75LeetCode.Services.Arrays._07_MinInRotatedSortedArray;
+using Shouldly;
+
+namespace Blind75LeetCode.UnitTests.Arrays._07_MinInRotatedSortedArray;
+public class MinInRotatedSortedArrayTests
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void BruteForceTests(int[] nums, int answer)
+    {
+        // Arrange
+        // Act
+        var result = MinInRotatedSortedArrayService.BruteForce(nums);
+
+        // Assert
+        result.ShouldBe(answer);
+    }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 0 },
+        };
+}
