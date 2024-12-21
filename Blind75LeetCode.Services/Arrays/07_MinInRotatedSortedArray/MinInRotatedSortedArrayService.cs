12897c5c6478c63c35e451bb4b2da70992e815fb
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-21 16:08:46 +1100

    getting things setup

--- Blind75LeetCode.Services/Arrays/07_MinInRotatedSortedArray/MinInRotatedSortedArrayService.cs
+++ Blind75LeetCode.Services/Arrays/07_MinInRotatedSortedArray/MinInRotatedSortedArrayService.cs
@@ -3,10 +3,17 @@ public static class MinInRotatedSortedArrayService
 {
     public static int BruteForce(int[] nums)
     {
-        for (int i = 0; i < nums.Length; i++)
-        {
+        // { 6, 7, 8, 9, 0, 1, 2, 3, 4, 5 }
+        // len = 10;
+        var left = nums[0];
+        var right = nums[nums.Length - 1];
+        var mid = nums.Length / 2;
 
-        }
+        //while(true)
+        //{
+        //    // if 
+        //}
+
         return 0;
     }
 }
--- Blind75LeetCode.UnitTests/Arrays/07_MinInRotatedSortedArray/MinInRotatedSortedArrayTests.cs
+++ Blind75LeetCode.UnitTests/Arrays/07_MinInRotatedSortedArray/MinInRotatedSortedArrayTests.cs
@@ -19,6 +19,6 @@ public class MinInRotatedSortedArrayTests
     public static IEnumerable<object[]> Data =>
         new List<object[]>
         {
-            new object[] { new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 0 },
+            new object[] { new int[] { 6, 7, 8, 9, 0, 1, 2, 3, 4, 5 }, 0 },
         };
 }
