9538acf2629c5548d627df7481820421fa92c234
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-12-22 08:12:55 +1100

    put in place

---
+++ Blind75LeetCode.Services/Arrays/08_SearchInRotatedSortedArray/SearchInRotatedSortedArrayService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blind75LeetCode.Services.Arrays._08_SearchInRotatedSortedArray;
+
+public static class SearchInRotatedSortedArrayService
+{
+    public static int BruteForce(int[] nums, int target)
+    {
+        var search = nums[0];
+        for (int i = 0; i < nums.Length; i++)
+        {
+        }
+
+        return search;
+    }
+}
---
+++ Blind75LeetCode.UnitTests/Arrays/08_SearchInRotatedSortedArray/SearchInRotatedSortedArrayTests.cs
@@ -0,0 +1,26 @@
+﻿using Blind75LeetCode.Services.Arrays._08_SearchInRotatedSortedArray;
+using Shouldly;
+
+namespace Blind75LeetCode.UnitTests.Arrays._08_SearchInRotatedSortedArray;
+public class SearchInRotatedSortedArrayTests
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void BruteForceTests(int[] nums, int target, int answer)
+    {
+        // Arrange
+        // Act
+        var result = SearchInRotatedSortedArrayService.BruteForce(nums, target);
+
+        // Assert
+        result.ShouldBe(answer);
+    }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0, 4 },
+            new object[] { new int[] { 4, 5, 6, 7, 0, 1, 2 }, 3, -1 },
+            new object[] { new int[] { 1 }, 0, -1 },
+        };
+}
